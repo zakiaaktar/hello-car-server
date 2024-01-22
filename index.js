736a1c90e7cc273a553cd561814fcccb6e76dbf9
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2024-01-23 00:46:39 +0100

    product details api part done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
@@ -38,6 +38,13 @@ async function run() {
         const cursor = productCollection.find()
         const result = await cursor.toArray()
         res.send(result)
+    });
+
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
     });
 
 
