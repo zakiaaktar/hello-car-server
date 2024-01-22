66eff6a3ecf75361f09527c3ebd93f0736e9765d
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2024-01-23 00:00:39 +0100

    get products api part done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-//console.log(process.env.DB_USER);
+console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2meybxt.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,16 +30,27 @@ async function run() {
     await client.connect();
 
 
+    const productCollection = client.db('carDb').collection('products');
+
+
+    // products related apis
+    app.get('/products', async (req, res) => {
+        const cursor = productCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    });
+
+
 
     
 
 
     // Send a ping to confirm a successful connection
-    //await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
